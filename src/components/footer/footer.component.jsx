99b6042590dd6a3af505fb1bea56e824782f40c6
Mohammad Maninang <mohammadmaninang94@gmail.com>
2020-08-24 08:08:39 +0800

    Added footer in home page

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import CustomLink from '../custom-button/custom-button.component';
 
+import './footer.styles.css';
+
 const Footer = () => (
     <footer>
         <div className='row'>
@@ -10,11 +12,21 @@ const Footer = () => (
                     <li><CustomLink htref='#'>About us</CustomLink></li>
                     <li><CustomLink htref='#'>Blog</CustomLink></li>
                     <li><CustomLink htref='#'>Press</CustomLink></li>
-                    <li><CustomLink htref='#'>ios App</CustomLink></li>
+                    <li><CustomLink htref='#'>iOS App</CustomLink></li>
                     <li><CustomLink htref='#'>Android App</CustomLink></li>
                 </ul>
             </div>
-            <div className='col span-1-of-2'></div>
+            <div className='col span-1-of-2'>
+                <ul className='social-links'>
+                    <li><CustomLink href='#'><i className='ion-social-facebook'></i></CustomLink></li>
+                    <li><CustomLink href='#'><i className='ion-social-twitter'></i></CustomLink></li>
+                    <li><CustomLink href='#'><i className='ion-social-googleplus'></i></CustomLink></li>
+                    <li><CustomLink href='#'><i className='ion-social-instagram'></i></CustomLink></li>
+                </ul>
+            </div>
+        </div>
+        <div className='row'>
+            <p>Copyright &copy; {new Date().getFullYear()} by Omnifood. All rigths reserved.</p>
         </div>
     </footer>
 );
